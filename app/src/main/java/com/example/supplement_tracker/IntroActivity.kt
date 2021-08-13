82b4b8e4dc849c4463863e841bd04387a25a381c
BTRA3 <mh1634@gmail.com>
2021-08-14 00:24:36 +0900

    first commit

---
+++ app/src/main/java/com/example/supplement_tracker/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.example.supplement_tracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import java.time.Duration
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },1500)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
--- app/src/main/java/com/example/supplement_tracker/MainActivity.kt
+++ app/src/main/java/com/example/supplement_tracker/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.supplement_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.supplement_tracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)  }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
